254af29e6da2771f951963cfae68ba3f9e44ae14
杨小刚 <nulijiuhao712@126.com>
2020-08-28 14:45:28 +0800

    project init

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,12 +4,12 @@ module.exports = function(app) {
     '/admin',
     createProxyMiddleware({
       // mock api
-      // target: 'http://localhost:' + process.env.REACT_APP__MOCK_SERVER_PORT,
-      // pathRewrite: { '^/admin': '' },
+      target: 'http://localhost:' + process.env.REACT_APP__MOCK_SERVER_PORT,
+      pathRewrite: { '^/admin': '' },
       // online api
-      target: 'http://vadmtest.eyangmedia.com/',
-      changeOrigin: true,
-    }),
+      // target: 'http://vadmtest.eyangmedia.com/',
+      // changeOrigin: true,
+    })
   );
   app.use(
     '/api',
@@ -20,6 +20,6 @@ module.exports = function(app) {
         Authorization:
           'bearer qE2JSWuxoUfg2CdtEbwzvZAtit9_reuoh3NYDA_LTjrABrxg3yD-wA5PoYc3D7WQkb67GcnIknma3Fz3bsCiRCsdPyaSwbJ44FKRMTCMWMxwXPwEdklxMxHji_JZOz0XsUtZyb321zoF-RnRQzDWWMBWShUWdUnwymZXm8WRsCfnUwOy-iEibUidqvV1YEE4isqdpHtmF6Y-j7CUfEvqmGEC2ZLNO8PgnEQTFfIu7aJowXtZCeGCnJIJP5MEa1YpAAv_wWrvqmhKQVDeGAqhluDUaeeR6-QAFwGE3ESu5dE',
       },
-    }),
+    })
   );
 };
