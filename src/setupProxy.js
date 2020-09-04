a30088b37dfba9852958e0f0778cbfb6a138ba9b
杨小刚 <nulijiuhao712@126.com>
2020-09-04 16:20:57 +0800

    update project over

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,10 +5,6 @@ module.exports = function(app) {
     createProxyMiddleware({
       target: 'https://clinic.jschgcp.cn',
       changeOrigin: true,
-      headers: {
-        Authorization:
-          'bearer qE2JSWuxoUfg2CdtEbwzvZAtit9_reuoh3NYDA_LTjrABrxg3yD-wA5PoYc3D7WQkb67GcnIknma3Fz3bsCiRCsdPyaSwbJ44FKRMTCMWMxwXPwEdklxMxHji_JZOz0XsUtZyb321zoF-RnRQzDWWMBWShUWdUnwymZXm8WRsCfnUwOy-iEibUidqvV1YEE4isqdpHtmF6Y-j7CUfEvqmGEC2ZLNO8PgnEQTFfIu7aJowXtZCeGCnJIJP5MEa1YpAAv_wWrvqmhKQVDeGAqhluDUaeeR6-QAFwGE3ESu5dE',
-      },
     })
   );
   app.use(
