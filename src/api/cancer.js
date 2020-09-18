a4fef00080307b7dd5590a1468e21b820f6fe4e6
杨小刚 <nulijiuhao712@126.com>
2020-09-18 16:52:33 +0800

    cancer manage api

--- src/api/cancer.js
+++ src/api/cancer.js
@@ -6,3 +6,41 @@ export function fetchCancersApi() {
     method: 'get',
   });
 }
+
+export function fetchCancerListApi(params = {}) {
+  return request({
+    url: `${process.env.REACT_APP_REQUEST_BASE_URL}/cancer/search`,
+    method: 'get',
+    params,
+  });
+}
+
+export function addCancerApi(data = {}) {
+  return request({
+    url: `${process.env.REACT_APP_REQUEST_BASE_URL}/cancer`,
+    method: 'post',
+    data,
+  });
+}
+
+export function fetchCancerDetailApi(cancer_id = '') {
+  return request({
+    url: `${process.env.REACT_APP_REQUEST_BASE_URL}/cancer/${cancer_id}`,
+    method: 'get',
+  });
+}
+
+export function updateCancerApi(cancer_id, data = {}) {
+  return request({
+    url: `${process.env.REACT_APP_REQUEST_BASE_URL}/cancer/${cancer_id}`,
+    method: 'put',
+    data,
+  });
+}
+
+export function delCancerApi(cancer_id = '') {
+  return request({
+    url: `${process.env.REACT_APP_REQUEST_BASE_URL}/cancer/${cancer_id}`,
+    method: 'delete',
+  });
+}
