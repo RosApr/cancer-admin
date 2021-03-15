6aa04c77af1e286df0220481117a49c2fc532f79
杨小刚 <nulijiuhao712@126.com>
2021-03-15 10:12:38 +0800

    copyright

--- src/layouts/footer/index.js
+++ src/layouts/footer/index.js
@@ -3,11 +3,14 @@ import { Layout } from 'antd';
 import './index.scss';
 const { Footer } = Layout;
 
+const currentYear = new Date().getFullYear();
+
 export default function CopyRight() {
   return (
     <Footer className='copyright'>
-      Copyright© 2019-2020 江苏省肿瘤防治研究所 All Right Reserved.
-      江苏省肿瘤防治研究所(江苏省肿瘤医院) 版权所有 - 京ICP备16027532号-1
+      `Copyright© ${currentYear}-${currentYear + 1} 江苏省肿瘤防治研究所 All
+      Right Reserved. 江苏省肿瘤防治研究所(江苏省肿瘤医院) 版权所有 -
+      京ICP备16027532号-1`
     </Footer>
   );
 }
